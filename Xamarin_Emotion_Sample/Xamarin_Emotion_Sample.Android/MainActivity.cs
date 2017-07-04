e628e688485753f6d1f348009cb1fb3c50c313a9
Fumiya Kume <fumiya.kume@hotmail.com>
2017-07-04 23:31:38 +0900

    Support Android 7.0

--- Xamarin_Emotion_Sample/Xamarin_Emotion_Sample.Android/MainActivity.cs
+++ Xamarin_Emotion_Sample/Xamarin_Emotion_Sample.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Permissions;
 
 namespace Xamarin_Emotion_Sample.Droid
 {
@@ -21,6 +22,11 @@ namespace Xamarin_Emotion_Sample.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
